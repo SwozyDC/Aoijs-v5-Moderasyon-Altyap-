1e19392f697b3ed01b18cb10b1c9f2c8af2c09e2
Glitch (glitch-hello-node) <none>
2022-04-02 09:57:44 +0000

    🎉🗼 Checkpoint ./LICENSE.md:32669520/1065 ./server.js:32669520/620 ./package.json:32669520/184 ./database/main/main_scheme_1.sql.js:32669520/8 ./komutlar/eval.js:32669520/294

---
+++ komutlar/eval.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "eval",
+  code: `
+$eval[$message]
+$onlyForIDs[$botOwnerID;**$customEmoji[emoji_71] | Bu Komut Sadece \`$userTag[728155399655784488]\`'a Özeldir**]
+`,
+};
